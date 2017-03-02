df83e656451d185a3f1e00a7c407cd176812bab6
Alejandro Anzola Avila <anzola95avila@gmail.com>
2017-03-02 12:59:45 -0500

    agrega especificacion clases de equivalencia de registrarCliente e implementacion de una

--- src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
@@ -5,7 +5,9 @@
  */
 package edu.eci.pdsw.samples.tests;
 
+import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -16,6 +18,12 @@ import static org.junit.Assert.*;
  */
 public class ClientesTest {
 
+    /**
+     * Clases de Equivalencia: metodo registrarCliente de ServiciosAlquiler
+     * CE1: Cliente, donde es la primera vez que se agrega TIPO: Normal. Resultado Esperado: se agrega el cliente
+     * CE2: Cliente, donde ya fue agregado previamente TIPO: Error. Resultado Esperado: ExcepcionServiciosAlquiler
+     */
+    
     public ClientesTest() {
     }
     
@@ -23,10 +31,22 @@ public class ClientesTest {
     public void setUp() {
     }
     
-  
-    @Test
-    public void additems1() throws ExcepcionServiciosAlquiler{
-    	
+    /**
+     * 
+     * @throws ExcepcionServiciosAlquiler 
+     */
+    @Test(expected=ExcepcionServiciosAlquiler.class)
+    public void CE2Test() throws ExcepcionServiciosAlquiler{
+    	Cliente c = new Cliente("Alejandro Anzola Avila", 1019115165, "304 602 3862",
+        "Suba Pinar Calle XXX A No YY - ZZ Casa 333", "alejandro.anzola@mail.escuelaing.edu.co");
+        
+        ServiciosAlquiler sa = ServiciosAlquiler.getInstance();
+        
+        sa.registrarCliente(c);
+        
+        sa.registrarCliente(c);
+        
+        fail("El servicio de alquiler no lanzo excepcion ExcepcionServiciosAlquiler");
     }
     
     
