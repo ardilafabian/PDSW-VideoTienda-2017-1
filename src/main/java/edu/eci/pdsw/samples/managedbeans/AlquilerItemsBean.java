99ecf416aece8d5d21b23fb54d428551e705751b
Fabian Ardila <fabianra97@gmail.com>
2017-03-14 20:59:29 -0500

    Implementa alquiler item

--- src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -6,6 +6,7 @@
 package edu.eci.pdsw.samples.managedbeans;
 
 import com.sun.media.jfxmedia.logging.Logger;
+import edu.eci.pdsw.samples.entities.Item;
 import edu.eci.pdsw.samples.entities.ItemRentado;
 import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
@@ -14,16 +15,19 @@ import java.time.*;
 import java.sql.Date;
 import java.util.*;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author 2106913
  */
 @ManagedBean(name = "AlquilerItems")
-@RequestScoped
+@ViewScoped
 public class AlquilerItemsBean implements Serializable {
 
     @ManagedProperty(value="#{dtClientes}")
@@ -37,8 +41,8 @@ public class AlquilerItemsBean implements Serializable {
     private int rentDays;
     
     public AlquilerItemsBean() {
-        Logger.logMsg(Logger.DEBUG, "Id del cliente: "+clientId);
-        Logger.logMsg(Logger.DEBUG, "Se insancia " + this.getClass().getName());
+        Logger.logMsg(Logger.DEBUG, "Id del cliente: "+clientId+" en "+this.getClass().getName());
+        Logger.logMsg(Logger.DEBUG, "Se instancia " + this.getClass().getName());
     }
     
     @PostConstruct
@@ -46,7 +50,7 @@ public class AlquilerItemsBean implements Serializable {
         Logger.logMsg(Logger.DEBUG, "Se ejecuta metodo init de post construccion");
         if (clientBean != null) {
             this.clientId = clientBean.getClientId();
-            Logger.logMsg(Logger.DEBUG, "Se obtiene el id del cliente de clienteBean: " + clientId);
+            Logger.logMsg(Logger.DEBUG, "Se obtiene el id del cliente de clienteBean: " + clientId+" en "+this.getClass().getName());
         } else {
             Logger.logMsg(Logger.ERROR, "Bean de cliente es null");
         }
@@ -73,7 +77,21 @@ public class AlquilerItemsBean implements Serializable {
     }
     
     public void setRentedItem(int id) {
-        this.itemId = id;
+        List<Item> itds = sp.consultarItemsDisponibles();
+        boolean is = false;
+        for (Item i : itds) {
+            if (i.getId() == id) {
+                is = true;
+            }
+        }
+        Logger.logMsg(Logger.DEBUG, "El item esta disponible: "+is);
+        if (is) {
+            Logger.logMsg(Logger.DEBUG, "Intenta asignar id del item: "+ id);
+            this.itemId = id;
+            Logger.logMsg(Logger.DEBUG, "Asigna ID del item: " + itemId );
+        } else {
+            FacesContext.getCurrentInstance().addMessage("noId", new FacesMessage(FacesMessage.SEVERITY_ERROR, "!Error!", "Item no disponible."));
+        }
     }
     
     public int getRentedItem() {
@@ -81,6 +99,7 @@ public class AlquilerItemsBean implements Serializable {
     }
     
     public void setDate(int days) throws ExcepcionServiciosAlquiler {
+        Logger.logMsg(Logger.DEBUG, "Intenta asignar dias de prestamo" + days +" para el item "+ itemId);
         this.rentDays = days;
         this.rentCost = sp.consultarCostoAlquiler(itemId, days);
         Logger.logMsg(Logger.DEBUG, "Agrega costo de la renta "+rentCost);
@@ -91,7 +110,11 @@ public class AlquilerItemsBean implements Serializable {
     }
     
     public long getRentCost() {
+        Logger.logMsg(Logger.DEBUG, "Consulta el costo de la renta");
         return rentCost;
+    }
+    
+    public void setRentCost(long rent) {
     }
     
     public void alquilarItem() throws ExcepcionServiciosAlquiler {
